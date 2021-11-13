5a88f2aa2ae85b14b2e0c4168f60e6fcbd5d41ef
Krazune <krazune@gmail.com>
2021-11-13 11:02:22 +0000

    Add coordinate rounding tests

--- src/test/java/com/sextantlocator/SextantCoordinateTest.java
+++ src/test/java/com/sextantlocator/SextantCoordinateTest.java
@@ -60,4 +60,90 @@ public class SextantCoordinateTest
 		assertTrue(coordinate.getMinutes() == 30f);
 		assertTrue(coordinate.getDirection() == Direction.EAST);
 	}
+
+	@Test
+	public void positiveCoordinatesRoundingTest()
+	{
+		SextantCoordinate coordinate;
+
+		// Good...
+		coordinate = new SextantCoordinate(3069f, SextantAxis.HORIZONTAL);
+
+		assertTrue(coordinate.getDegrees() == 19f);
+		assertTrue(coordinate.getMinutesInteger() == 39f);
+		assertTrue(coordinate.getDirection() == Direction.EAST);
+
+		coordinate = new SextantCoordinate(3225f, SextantAxis.HORIZONTAL);
+
+		assertTrue(coordinate.getDegrees() == 24f);
+		assertTrue(coordinate.getMinutesInteger() == 31f);
+		assertTrue(coordinate.getDirection() == Direction.EAST);
+
+		coordinate = new SextantCoordinate(3178f, SextantAxis.HORIZONTAL);
+
+		assertTrue(coordinate.getDegrees() == 23f);
+		assertTrue(coordinate.getMinutesInteger() == 3f);
+		assertTrue(coordinate.getDirection() == Direction.EAST);
+
+		// ... luck
+		coordinate = new SextantCoordinate(3517f, SextantAxis.VERTICAL);
+
+		assertTrue(coordinate.getDegrees() == 11f);
+		assertTrue(coordinate.getMinutesInteger() == 7f);
+		assertTrue(coordinate.getDirection() == Direction.NORTH);
+
+		coordinate = new SextantCoordinate(3219f, SextantAxis.VERTICAL);
+
+		assertTrue(coordinate.getDegrees() == 1f);
+		assertTrue(coordinate.getMinutesInteger() == 48f);
+		assertTrue(coordinate.getDirection() == Direction.NORTH);
+
+		coordinate = new SextantCoordinate(3475f, SextantAxis.VERTICAL);
+
+		assertTrue(coordinate.getDegrees() == 9f);
+		assertTrue(coordinate.getMinutesInteger() == 48f);
+		assertTrue(coordinate.getDirection() == Direction.NORTH);
+	}
+
+	@Test
+	public void negativeCoordinatesRoundingTest()
+	{
+		SextantCoordinate coordinate;
+
+		coordinate = new SextantCoordinate(2210f, SextantAxis.HORIZONTAL);
+
+		assertTrue(coordinate.getDegrees() == 7f);
+		assertTrue(coordinate.getMinutesInteger() == 11f);
+		assertTrue(coordinate.getDirection() == Direction.WEST);
+
+		coordinate = new SextantCoordinate(2152f, SextantAxis.HORIZONTAL);
+
+		assertTrue(coordinate.getDegrees() == 9f);
+		assertTrue(coordinate.getMinutesInteger() == 0f);
+		assertTrue(coordinate.getDirection() == Direction.WEST);
+
+		coordinate = new SextantCoordinate(2377f, SextantAxis.HORIZONTAL);
+
+		assertTrue(coordinate.getDegrees() == 1f);
+		assertTrue(coordinate.getMinutesInteger() == 58f);
+		assertTrue(coordinate.getDirection() == Direction.WEST);
+
+		coordinate = new SextantCoordinate(2855f, SextantAxis.VERTICAL);
+
+		assertTrue(coordinate.getDegrees() == 9f);
+		assertTrue(coordinate.getMinutesInteger() == 33f);
+		assertTrue(coordinate.getDirection() == Direction.SOUTH);
+
+		coordinate = new SextantCoordinate(3072f, SextantAxis.VERTICAL);
+
+		assertTrue(coordinate.getDegrees() == 2f);
+		assertTrue(coordinate.getMinutesInteger() == 46f);
+		assertTrue(coordinate.getDirection() == Direction.SOUTH);
+
+		coordinate = new SextantCoordinate(3036f, SextantAxis.VERTICAL);
+
+		assertTrue(coordinate.getDegrees() == 3f);
+		assertTrue(coordinate.getMinutesInteger() == 54f);
+		assertTrue(coordinate.getDirection() == Direction.SOUTH);
+	}
 }
